70323522436f8b9aadba19d384984ed477dffaff
franc <fmenendezwork@gmail.com>
2023-08-30 23:15:20 -0700

    Create Search component. Basic search bar for the home page using styled-components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Recipe App</h1>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
