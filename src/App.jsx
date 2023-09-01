eb964728a433091becfff33cf2499c9d0960eb2a
Roberto Rodriguez <jrhitman5150@gmail.com>
2023-08-31 20:31:26 -0700

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Search from "./components/Search";
+import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <h1>Recipe App</h1>
       <Search />
+      <Category/>
+      <Pages/>
+      </BrowserRouter>
     </div>
   );
 }
