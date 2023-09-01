647673e7302b4afaf72251cc667335c63d7fb8c2
Roberto Rodriguez <jrhitman5150@gmail.com>
2023-08-31 18:57:30 -0700

    Making category components

---
+++ src/components/Category.jsx
@@ -0,0 +1,13 @@
+import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
+import { GiNoodles, GiChopsticks } from "react-icons/gi";
+import styled from "styled-components";
+
+function Category() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Category
