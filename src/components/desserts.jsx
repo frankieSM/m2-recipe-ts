1038641392da75e4cb0361b7d1b3681559a004b7
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-08-31 18:24:08 -0700

    Fixed desserts and app.js conflicts

---
+++ src/components/desserts.jsx
@@ -0,0 +1,5 @@
+function Desserts() {
+    return <div>Desserts</div>;
+  }
+  
+  export default Desserts;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,15 @@
 import Popular from "../components/popular";
-import Desserts from "../components/desserts"
+import Desserts from "../components/desserts";
 
-import React from 'react'
+import React from "react";
 
 function Home() {
-    return (
-        <div>
-            <Popular />\
-            <Desserts />
-
-        </div>
-    )
+  return (
+    <div>
+      <Popular />\
+      <Desserts />
+    </div>
+  );
 }
 
 export default Home;
