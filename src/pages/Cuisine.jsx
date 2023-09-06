6de1c4c4db5a4e763b5ea116aad8da8e7933ce8a
Roberto Rodriguez <jrhitman5150@gmail.com>
2023-09-05 20:24:23 -0700

    Update page

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import {Link, useParams } from "react-router-dom";
 
 
