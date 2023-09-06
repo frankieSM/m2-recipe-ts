b8d1ff5d225a0e1fc71df9acb5c0628f05282b6b
Francisco Menendez <fmenendezwork@gmail.com>
2023-09-05 21:33:13 -0700

    Edit App.js with more components. Errros in icons.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import Search from "./pages/Search";
+import Category from "./components/Category";
 import Pages from "./pages/Pages";
 
 function App() {
@@ -7,6 +8,8 @@ function App() {
     <div className="App">
       <h1>Recipe App</h1>
       <BrowserRouter>
+        <Search />
+        <Category />
         <Pages />
       </BrowserRouter>
     </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,11 @@
-import Search from "../components/Search";
 import Popular from "../components/Popular";
-import Desserts from "../components/desserts";
+import Desserts from "../components/Desserts";
 
 import React from "react";
 
 function Home() {
   return (
     <div>
-      <Search />
       <Popular />
       <Desserts />
     </div>
@@ -15,7 +13,3 @@ function Home() {
 }
 
 export default Home;
-
-//When pages components are finished
-//render out <Pages /> instead of
-//individual components
