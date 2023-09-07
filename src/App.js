15c90912821e0ab19a0897449c4a280de624eadb
Francisco Menendez <fmenendezwork@gmail.com>
2023-09-06 20:10:43 -0700

    Save before checking main branch.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Pages from "./pages/Pages";
 function App() {
   return (
     <div className="App">
-      <h1>Recipe App</h1>
       <BrowserRouter>
         <Search />
         <Category />
