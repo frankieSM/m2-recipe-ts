29daedd0133778d7bb17e01d8a4fc244cf5d0a1f
Francisco Menendez <fmenendezwork@gmail.com>
2023-09-06 22:13:28 -0700

    Heavy styling to Search.jsx and Searched.jsx. No link yet from searched items to Recipe view.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,7 +8,7 @@ function Search() {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    navigate("/searched/ + input");
+    navigate("/searched/" + input);
   };
 
   return (
@@ -24,24 +24,28 @@ function Search() {
 }
 
 const StyledForm = styled.form`
-  margin: 0rem 20rem;
-  position: relative;
-  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  max-width: 500px;
+
   input {
     border: none;
-    background: linear-gradient(35deg, #494949, #313131);
+    background: linear-gradient(35deg, #a32b21, #761d14);
     font-size: 1.5rem;
     color: white;
     padding: 1rem 3rem;
-    border: none;
     border-radius: 1rem;
     outline: none;
+    flex: 1;
   }
-  svg {
-    position: absolute;
-    top: 50%;
-    left: 0%;
-    color: white;
+
+  .gradient-end {
+    flex: 0.1;
+    background: linear-gradient(to right, rgba(163, 43, 33, 0), #a32b21);
+    width: 100px;
+    height: 100%;
   }
 `;
 
--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import styled from "styled-components";
 
 function Searched() {
   const [searchedRecipes, setSearchedRecipes] = useState([]);
@@ -18,7 +19,65 @@ function Searched() {
     getSearched(params.search);
   }, [params.search]);
 
-  return <div>Searched</div>;
+  return (
+    <RecipeGrid>
+      {searchedRecipes.map((recipe) => (
+        <RecipeCardContainer key={recipe.id}>
+          <RecipeImage src={recipe.image} alt={recipe.title} />
+          <RecipeTitle>{recipe.title}</RecipeTitle>
+        </RecipeCardContainer>
+      ))}
+    </RecipeGrid>
+  );
 }
+
+const RecipeGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr); /* Three equal-width columns */
+  gap: 16px; /* Spacing between cards */
+`;
+
+const RecipeCardContainer = styled.div`
+  width: 300px;
+  margin: 16px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+const RecipeImage = styled.img`
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  position: relative;
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(
+      to bottom,
+      transparent 0%,
+      rgba(0, 0, 0, 0.6) 100%
+    );
+    opacity: 0;
+    transition: opacity 0.3s;
+  }
+
+  &:hover:before {
+    opacity: 1;
+  }
+`;
+
+const RecipeTitle = styled.h3`
+  font-size: 1.2rem;
+  padding: 16px;
+  text-align: center;
+  color: #333;
+`;
 
 export default Searched;
