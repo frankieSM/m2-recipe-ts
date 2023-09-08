386540c7bed24877639a4cfed948a901b96d2bbd
Roberto Rodriguez <jrhitman5150@gmail.com>
2023-09-07 23:15:56 -0700

    updated api key

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Search from "./components/Search";
 import Category from "./components/Category";
 import Pages from "./pages/Pages";
 
+
 function App() {
   return (
     <div className="App">
--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -56,6 +56,7 @@ function Recipe() {
         ;
       </Info>
     </DetailWrapper>
+    
   );
 }
 
